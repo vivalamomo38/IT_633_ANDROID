35b578cc37840555f62b02f61561e0e7682de340
Sean Moran <smoran38@gmail.com>
2020-07-22 19:11:36 -0400

    Initial Login UI create animation
    Initial Login UI Create animation --
    Tutorial Source was found here
    by Angga Risky
    https://youtu.be/JqPv4LZ53rU
    
    TO:DO modify to do the same for a contiue as guest button

--- AcmeDepartmentStore/app/src/main/java/com/example/acmedepartmentstore/MainActivity.java
+++ AcmeDepartmentStore/app/src/main/java/com/example/acmedepartmentstore/MainActivity.java
@@ -1,15 +1,142 @@
 package com.example.acmedepartmentstore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.ViewGroup;
+
+import com.example.acmedepartmentstore.databinding.ActivityMainBinding;
+import com.example.acmedepartmentstore.ui.login.LoginActivity;
+
+import java.util.concurrent.Delayed;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBinding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+    }
 
+    public void load(View view){
+
+
+        //Call private method animateButtonWidth
+        animateButtonWidth();
+        fadeOutTextAndSetProgressDialog();
+        nextAction();
+
+    }
+
+    private void animateButtonWidth(){
+
+        ValueAnimator anim = ValueAnimator.ofInt(mBinding.signInBtn.getMeasuredWidth());
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+
+                int value = (Integer) animation.getAnimatedValue();
+                ViewGroup.LayoutParams layoutParams = mBinding.signInBtn.getLayoutParams();
+                layoutParams.width=value;
+                mBinding.signInBtn.requestLayout();
+            }
+        });
+
+
+        anim.setDuration(250);
+        anim.start();
+    }
+
+    private void fadeOutTextAndSetProgressDialog(){
+        mBinding.signInTxt.animate().alpha(0f).setDuration(250).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                // When Text is GONE call the progress Dialog
+                showProgressDialog();
+            }
+        }).start();
+    }
+
+
+    //SHOW PROGRESS DIALOG METHOD
+    private void showProgressDialog(){
+
+        mBinding.progressBar.getIndeterminateDrawable().setColorFilter(Color.parseColor("#ffffff"), PorterDuff.Mode.SRC_IN);
+        mBinding.progressBar.setVisibility(View.VISIBLE);
+
+    }
+
+    private void nextAction(){
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                revealButton();
+                fadeOutProgressDialog();
+                delayedStartNextActivity();
+
+            }
+        }, 2000);
+    }
+
+    private void revealButton(){
+        mBinding.signInBtn.setElevation(0f);
+        mBinding.revealView.setVisibility(View.VISIBLE);
+
+        int x = mBinding.revealView.getWidth();
+        int y = mBinding.revealView.getHeight();
+
+        int startX = (int) (getFinalWidth()/ 2 + mBinding.signInBtn.getX());
+        int startY = (int) (getFinalWidth()/ 2 + mBinding.signInBtn.getY());
+
+        float radius = Math.max(x,y) * 1.2f;
+
+        Animator reveal = ViewAnimationUtils.createCircularReveal(mBinding.revealView,startX,startY,getFinalWidth(),radius);
+        reveal.setDuration(350);
+        reveal.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                finish();
+            }
+        });
+
+        reveal.start();
+    }
+
+    // Fade Out Progress Dialog
+
+    private void fadeOutProgressDialog(){
+        mBinding.progressBar.animate().alpha(0f).setDuration(200).start();
+    }
+
+    private void delayedStartNextActivity(){
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+
+            }
+        },100);
+
+    }
+
+    // GET RESOURCE WIDTH FOR BUTTON
+    private int getFinalWidth (){
+
+        return (int) getResources().getDimension(R.dimen.get_width);
     }
 }
