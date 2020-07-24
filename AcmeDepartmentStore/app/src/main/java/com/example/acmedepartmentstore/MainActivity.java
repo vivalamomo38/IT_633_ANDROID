7e1f5283536f47cc23baf63eb66b7e8af643a504
Sean Moran <smoran38@gmail.com>
2020-07-24 01:14:23 -0400

    Login Activity & Firebase Auth
    - Initial Pass at cleaning up Login
      activity
    - Provided login functionality
      with firebase.
    - Setup up AuthCheck in mainActivity
      for saved state to send to Home
      screen instead

--- AcmeDepartmentStore/app/src/main/java/com/example/acmedepartmentstore/MainActivity.java
+++ AcmeDepartmentStore/app/src/main/java/com/example/acmedepartmentstore/MainActivity.java
@@ -17,14 +17,41 @@ import androidx.databinding.DataBindingUtil;
 
 import com.example.acmedepartmentstore.databinding.ActivityMainBinding;
 import com.example.acmedepartmentstore.ui.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding mBinding;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        // Test to see if currentUser is null else go to home
+
+        if(currentUser !=null){
+            // Call private method to animateGuestButtonWidth and send user to Home Activity
+            startActivity(new Intent(MainActivity.this, Home.class));
+
+
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mBinding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+    }
+    public void guestLogin(View view){
+
+        // Call private method to animateGuestButtonWidth and send user to Home Activity
+        animateButtonWidth(0);
+        fadeOutTextAndSetProgressDialog();
+        nextAction(0);
+
+
     }
 
     public void loadLogin(View view){
@@ -36,15 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
-    public void guestLogin(View view){
 
-        // Call private method to animateGuestButtonWidth and send user to Home Activity
-        animateButtonWidth(0);
-        fadeOutTextAndSetProgressDialog();
-        nextAction(0);
-
-
-    }
 
     private void animateButtonWidth(int button){
         if(button==1){
@@ -64,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         anim.setDuration(250);
-        anim.start();}else{
+        anim.start();}else if (button==0) {
             // Code to execute guest button login
             ValueAnimator anim = ValueAnimator.ofInt(mBinding.guestBtn.getMeasuredWidth());
             anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
@@ -76,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
                     layoutParams.width=value;
                     mBinding.guestBtn.requestLayout();
                     mBinding.guestBtn.setVisibility(View.INVISIBLE);
-                    mBinding.signInBtn.setVisibility(View.VISIBLE);
+
                 }
             });
 
