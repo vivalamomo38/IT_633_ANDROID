b82fb935678c9f9fb1b2f106441f4fa3dac7ac9a
Sean Moran <smoran38@gmail.com>
2020-07-31 10:55:18 -0400

    Initial Navigation Drawer
    - Begin implementation of navigation
      drawer in activity_home
    - Removed actionbar from style sheet to enable nav drawer
    - Added icons to res folder from https://www.youtube.com/watch?v=VD3YItr9nMg

--- AcmeDepartmentStore/app/src/main/java/com/example/acmedepartmentstore/Home.java
+++ AcmeDepartmentStore/app/src/main/java/com/example/acmedepartmentstore/Home.java
@@ -1,14 +1,55 @@
 package com.example.acmedepartmentstore;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class Home extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolBar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        toolBar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolBar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolBar,
+                R.string.openNavDrawer,
+                R.string.closeNavDrawer
+        );
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
